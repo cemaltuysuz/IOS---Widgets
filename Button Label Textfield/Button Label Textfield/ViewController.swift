4e9545db5990d52b60e46e3a76235656925d8f3c
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-22 17:56:31 +0300

    button,label and textfield widgets added.

---
+++ Button Label Textfield/Button Label Textfield/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Button Label Textfield
+//
+//  Created by cemal tüysüz on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text: UILabel!
+    
+    @IBOutlet weak var error: UILabel!
+    @IBOutlet weak var input: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Error message hidden
+        error.isHidden = true
+    }
+
+    @IBAction func button(_ sender: Any) {
+        
+        // Get İnput Number
+        if let inputNum = input.text{
+            // Get label number
+            if let labelNum = text.text {
+                // Convert to Int
+                if let conNum = Int(inputNum) {
+                    text.text = String(Int(labelNum)! + conNum)
+                    if !error.isHidden {
+                        error.isHidden = true
+                    }
+                }else{
+                    error.isHidden = false
+                }
+            }
+        }
+        
+    }
+    
+}
+
